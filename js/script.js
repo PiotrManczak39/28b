2154a989516e1f245dfdf0c4a28982ba0026d533
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2020-01-18 17:14:32 +0000

    Partials in scss
    Random bar functions.

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ Chart.pluginService.register({
 });
 
 let randomNumber = () => {
-  return Math.floor(Math.random() * 19) + 1;
+  return Math.floor(Math.random() * 28) + 1;
 }
 
 var config = {
@@ -100,47 +100,11 @@ var config1 = {
 var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, config);
 
-window.addEventListener('click', (event) => {
-  if (event.target.tagName == 'CANVAS') {
-    if (myChart.options.elements.center.text === '21/28') {
-        myChart = new Chart(ctx, config1);
+window.addEventListener('load', () => {
+    if (myChart.options.elements.center.text == '21/28') {
+      myChart.options.elements.center.text = randomNumber() + '/28';
+      myChart.update();
     } else {
-      myChart = new Chart(ctx, config);
+      myChart = new Chart(ctx, config1);
     }
-  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     type: 'doughnut',
-//     data: {
-        // labels: [],
-//         legend: {
-//           display: false
-//         },
-        // datasets: [{
-        //     label: 'My First dataset',
-        //     backgroundColor: 'orange',
-        //     borderColor: 'orange',
-        //     data: [75]
-        // }]
-//     },
-//     options: {
-      // cutoutPercentage: 87,
-      // rotation: 2 * Math.PI,
-      // circumference: 1.5 * Math.PI
-//     }
-// });
